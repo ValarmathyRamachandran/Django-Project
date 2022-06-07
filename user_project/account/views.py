b9371cddb11b68f57d41c6beb579d9175d5d1380
Valarmathy <valarmathy.ramachandran@outlook.com>
2022-06-07 21:15:41 +0530

    [Valarmathy]:Refactored registration Api and Login api with validations and exceptions using Django

--- user_project/account/views.py
+++ user_project/account/views.py
@@ -1,51 +1,77 @@
-import generics as generics
-from rest_framework.views import APIView, Response
-from rest_framework import generics
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
+from django.db.models import Q
+from django.http import HttpResponse
+from rest_framework.views import Response
 from .serializers import RegistrationSerializer, LoginSerializers
 from django.contrib.auth import get_user_model, authenticate, login
+from rest_framework import permissions, generics
 
 User = get_user_model()
 
 
 class RegistrationAPIView(generics.GenericAPIView):
+    permission_classes = ()
+    serializer_class = RegistrationSerializer
+    authentication_classes = ()
 
-    def post(self, request):
+    def post(self, request, *args, **kwargs):
         """
         post api is for user registration for registering new users
         :return: message after validations for adding new user
         """
         req_data = request.data
-        serializer = RegistrationSerializer(data=req_data)
+        serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        first_name = serializer.data.get('first_name')
-        last_name = serializer.data.get('last_name')
-        username = serializer.data.get('username')
-        email = serializer.data.get('email')
-        password = serializer.data.get('password')
 
-        user = User.objects.create_user(first_name=first_name, last_name=last_name,
-                                        username=username, email=email, password=password)
-        # if User.objects.filter(email=user.email):
-        # return Response({'msg': 'User Already exists'})
-        user.save()
-        return Response({'msg': 'User Registered successfully', 'code': 200})
+        first_name = req_data.get('first_name')
+        last_name = req_data.get('last_name')
+        username = req_data.get('username')
+        email = req_data.get('email')
+        password = req_data.get('password')
+        confirm_password = req_data.get('confirm_password')
+        try:
+            validate_email(email)
+        except ValidationError:
+            return Response('Please enter valid email')
+        query = User.objects.filter(
+            Q(username__iexact=username))
+
+        if query.exists():
+            return HttpResponse('user already exists')
+
+        if password != confirm_password:
+            return HttpResponse('password did not match')
+        try:
+            user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email)
+            print(user)
+            user.set_password(password)
+            user.is_active = False
+            user.save()
+            print("hi", user)
+            return HttpResponse("User Created Successfully", user)
+        except Exception as e:
+            return HttpResponse("Oops! Something went wrong, please try again later.", e)
 
 
-class LoginApiView(APIView):
-    permission_classes = ()
-    authentication_classes = ()
+class LoginApiView(generics.GenericAPIView):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = LoginSerializers
 
     def post(self, request):
-        data = request.data
-        serializer = LoginSerializers(data=data)
-        serializer.is_valid(raise_exception=True)
-        username = serializer.data.get('username')
-        password = serializer.data.get('password')
-        print(type(username))
-        print(password)
-        user = authenticate(request, username=username, password=password)
-        print(user)
-        if not user:
-            return Response({'Error': 'Invalid email or password', 'Code': 401})
-        login(request, user)
-        return Response({'msg': 'User Logged in Successfully', 'Code': 200})
+        try:
+            data = request.data
+            serializer = LoginSerializers(data=data)
+            serializer.is_valid(raise_exception=True)
+            username = data.get('username')
+            password = data.get('password')
+            user = authenticate(request, username=username, password=password)
+            # print(user)
+
+            if not user:
+                return Response({'Error': 'Invalid email or password', 'code': 401})
+            login(request, user)
+            return Response({'msg': 'User Logged in Successfully', 'code': 200})
+
+        except Exception:
+            return HttpResponse("Oops! Something went wrong, please try again later")
