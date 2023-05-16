95b21ed8b5cb2c034bf2ff89577013c60ca4a285
ValarmathyRamachandran <90239548+ValarmathyRamachandran@users.noreply.github.com>
2023-05-16 12:54:20 +0530

    Update settings.py

--- user_project/user_project/settings.py
+++ user_project/user_project/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'user_project',
         'USER': 'postgres',
-        'PASSWORD': 'valar@30',
+        'PASSWORD': '*******',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
